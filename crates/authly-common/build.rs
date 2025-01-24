0e6cc966cb700431962431a5e934ec4c26e0d30f
Audun Halland <audun.halland@pm.me>
2025-01-24 15:01:10 +0100

    feat: authly_connect protocol

--- crates/authly-common/build.rs
+++ crates/authly-common/build.rs
@@ -1,7 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile_protos(&["proto/authly_service.proto"], &["proto/"])?;
+        .compile_protos(
+            &["proto/authly_connect.proto", "proto/authly_service.proto"],
+            &["proto/"],
+        )?;
 
     println!("cargo:rerun-if-changed=build.rs");
 
--- crates/authly-common/src/proto.rs
+++ crates/authly-common/src/proto.rs
@@ -1,5 +1,10 @@
 //! Authly protobuf types.
 
+/// Tonic types for the `authly_connect` protobuf definition.
+pub mod connect {
+    tonic::include_proto!("authly_connect");
+}
+
 /// Tonic types for the `authly_service` protobuf definition.
 pub mod service {
     tonic::include_proto!("authly_service");
