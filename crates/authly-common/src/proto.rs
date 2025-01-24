c72183b575c021b3a3c998a2101f05dde05ea7b4
Audun Halland <audun.halland@pm.me>
2025-01-24 15:35:17 +0100

    feat: Authly Authority protocol

--- crates/authly-common/src/proto.rs
+++ crates/authly-common/src/proto.rs
@@ -1,6 +1,11 @@
 //! Authly protobuf types.
 
 /// Tonic types for the `authly_connect` protobuf definition.
+pub mod authority {
+    tonic::include_proto!("authly_authority");
+}
+
+/// Tonic types for the `authly_connect` protobuf definition.
 pub mod connect {
     tonic::include_proto!("authly_connect");
 }
