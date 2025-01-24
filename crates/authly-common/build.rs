b656a4b8c66b39e9704cf4db46f304a86bf2623a
Audun Halland <audun.halland@pm.me>
2025-01-24 18:18:14 +0100

    feat: Authly Mandate Submission

--- crates/authly-common/build.rs
+++ crates/authly-common/build.rs
@@ -2,7 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
-            &["proto/authly_connect.proto", "proto/authly_service.proto"],
+            &[
+                "proto/authly_connect.proto",
+                "proto/authly_mandate_submission.proto",
+                "proto/authly_service.proto",
+            ],
             &["proto/"],
         )?;
 
--- crates/authly-common/src/proto.rs
+++ crates/authly-common/src/proto.rs
@@ -5,6 +5,11 @@ pub mod connect {
     tonic::include_proto!("authly_connect");
 }
 
+/// Tonic types for the `authly_mandate_submission` protobuf definition.
+pub mod mandate_submission {
+    tonic::include_proto!("authly_mandate_submission");
+}
+
 /// Tonic types for the `authly_service` protobuf definition.
 pub mod service {
     tonic::include_proto!("authly_service");
